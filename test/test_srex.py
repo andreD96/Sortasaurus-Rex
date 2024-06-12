fda369888c231017ad9176b11a116b153341c2b2
Andrea D'Avino <46439698+andreD96@users.noreply.github.com>
2024-06-12 13:48:09 +0200

    Fix(): Unit test did not import the module correctly

--- test/test_srex.py
+++ test/test_srex.py
@@ -1,10 +1,13 @@
 """
 Unit testing file for classification script
 """
-
+import os
+import sys
 import unittest
 from unittest.mock import patch, MagicMock
 import srex
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 class TestFileClassification(unittest.TestCase):
